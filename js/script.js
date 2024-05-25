2682496b144752709437a45dc8a525ce189dc743
giopolub <giovanni.polub.orlandi@gmail.com>
2024-05-25 12:34:51 -0300

    Estilização e codificação páginas sucesso e erro bem como redirecionamentos adicionados.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,9 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    // Aqui você pode adicionar sua lógica de validação de login.
-    // Por enquanto, vamos apenas simular uma verificação simples.
     if (email === 'usuario@exemplo.com' && password === 'senha123') {
-        // Redirecionar para a página index.html se o login for bem-sucedido
-        window.location.href = 'index.html';
+        window.location.href = '../sucesso.html';
     } else {
-        // Exibir mensagem de erro se o login falhar
-        document.getElementById('error-message').style.display = 'block';
+        window.location.href = '../erro.html';
     }
 });
