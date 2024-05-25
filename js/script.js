f826e20f10ecd21da2cd1505d09a61fc638ab183
giopolub <giovanni.polub.orlandi@gmail.com>
2024-05-25 13:13:43 -0300

    Continuando script

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+if (!localStorage.getItem('userLoggedIn')) {
+    localStorage.setItem('userLoggedIn', JSON.stringify({ email: 'usuario@exemplo.com', password: 'senha123' }));
+}
+
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    if (email === 'usuario@exemplo.com' && password === 'senha123') {
-        window.location.href = '../sucesso.html';
+    const userLoggedIn = JSON.parse(localStorage.getItem('userLoggedIn'));
+
+    if (email === userLoggedIn.email && password === userLoggedIn.password) {
+        window.location.href = './sucesso.html';
     } else {
-        window.location.href = '../erro.html';
+        window.location.href = './erro.html';
     }
 });
