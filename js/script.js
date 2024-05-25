4ceab2e27bb8120f76bf636d7be3db8b0e3a01f3
giopolub <giovanni.polub.orlandi@gmail.com>
2024-05-25 12:20:13 -0300

    Tentativa implementação msgs de erro e sucesso.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,13 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
+    // Aqui você pode adicionar sua lógica de validação de login.
+    // Por enquanto, vamos apenas simular uma verificação simples.
     if (email === 'usuario@exemplo.com' && password === 'senha123') {
-        window.location.href = '../index.html';
+        // Redirecionar para a página index.html se o login for bem-sucedido
+        window.location.href = 'index.html';
     } else {
+        // Exibir mensagem de erro se o login falhar
         document.getElementById('error-message').style.display = 'block';
     }
 });
